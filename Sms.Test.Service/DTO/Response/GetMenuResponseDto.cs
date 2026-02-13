d433ce4e52ef7219638ffaf7f852d8708ab70b19
AndrewLityagin <trainee.d.swsyn@gmail.com>
2026-02-13 22:01:00 +0500

    Fix bugs

--- Sms.Test.Service/DTO/Response/GetMenuResponseDto.cs
+++ Sms.Test.Service/DTO/Response/GetMenuResponseDto.cs
@@ -4,5 +4,10 @@ public class GetMenuResponseDto
 {
     public bool Success { get; set; }
     public string ErrorMessage { get; set; }
-    public MenuItemDto[] MenuItems { get; set; }
+    public MenuData Data {get;set;}
+    
+}
+public  class MenuData
+{
+ public  MenuItemDto[] MenuItems {get;set;}
 }
